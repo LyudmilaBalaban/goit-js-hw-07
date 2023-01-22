b9155692384e139ec4bbed2c13d9b25b11a518af
LyudmilaBalaban <libalaban90@gmail.com>
2023-01-22 20:30:29 +0200

    completed task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    )
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.innerHTML = addGalleryMarkup;
+
+galleryContainer.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  blockStandartAction(e);
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(e) {
+  e.preventDefault();
+}
